270dec4b9ffd784e4cdd31ddc3304e59aaf05ffa
Neoloopy <neoloopybusiness@gmail.com>
2024-04-08 11:05:01 -0700

    Project Initialization
    Place basic components needed for development

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour, IMovement
+{
+    public void Move(){
+        Debug.Log("Player moved");
+        return;
+    }
+}
