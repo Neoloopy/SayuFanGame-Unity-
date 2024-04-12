4f3179c8c5f7f64243fec4e8681f033d00cc8c92
Neoloopy <neoloopybusiness@gmail.com>
2024-04-12 16:18:58 -0700

    Item Updates

---
+++ Assets/Scripts/Enums/StatusEffects.cs
@@ -0,0 +1,11 @@
+public enum StatusEffects
+{
+    Physical,
+    Fire,
+    Water,
+    Ice,
+    Nature,
+    Poison,
+    Wind,
+    Electric
+}
---
+++ Assets/Scripts/Items/ItemSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Items", menuName = "Items", order = 0)]
+public class ItemSO : BasicInfoSO
+{
+    public int weight;
+}
